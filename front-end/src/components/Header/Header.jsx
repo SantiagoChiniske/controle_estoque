c06aa797373e06bb6ae6135a122f8220160dc42d
Santiago Chiniske Pereira - Notebook <santiagochiniske10@gmail.com>
2026-02-18 16:30:19 -0300

    FEAT: Funcao de sair da aplicacao

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react"
 import Logo from "../../assets/Logo.svg"
 import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
 export default function  Header(){
+    const navigate = useNavigate()
     const roleToken = sessionStorage.getItem("role")
 
+    function sairDoSistema(e){
+        e.preventDefault();
+        sessionStorage.clear();
+        navigate("/login")
+
+    }
+    
 
     return(
         <aside className="w-55 h-screen  bg-black flex flex-col items-center">
@@ -23,6 +32,7 @@ export default function  Header(){
                  <Link className="text-center text-white font-light py-4 border-b-primary border-b-1 w-45 m-2  focus:text-primary focus:font-bold hover:text-primary " to="/listar/produtos">Listar itens</Link>
                 :<></>}
                 
+                <button onClick={sairDoSistema} className="text-white px-13 py-3 rounded-2xl border-2 border-primary cursor-pointer">Sair</button>
                
 
             </nav>
